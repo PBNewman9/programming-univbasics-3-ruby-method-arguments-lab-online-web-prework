5eb9bc8cb87ea8952e95f38f4f8097d011404fc2
Phil Newman <pnewman@viimed.com>
2019-10-25 17:50:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,15 +1,17 @@
 name = "Dan"
 language = "Ember.js"
 
-
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
 
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am leanring to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 def introduction_with_language(name, language = "Ruby")
-  puts "Hi, my name is #{name} and I am leanring to program #{language}."
+  puts "Hi, my name is #{name} and I am learning to program #{language}."
 end
 
 
